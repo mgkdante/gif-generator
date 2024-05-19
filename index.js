0b4888b797721ffc6471aa6a4f6d8b16a4711645
Yesid Otalora <mgkdante@pm.me>
2024-05-19 12:53:15 -0400

    Added error gif on catch

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const getGifButton = document.getElementById("get-gif");
 const searchGif = document.getElementById("search-gif");
 
 const getGif = (search = "Dog") => {
+  search = search === "" ? "Dog" : search;
   fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=MmSgiAlZf3wYCkNypp2Af66bGspaOTVF&s=${search}`,
     {
@@ -11,7 +12,12 @@ const getGif = (search = "Dog") => {
   )
     .then((response) => response.json())
     .then((response) => {
+      console.log(response);
       gif.src = response.data.images.original.url;
+    })
+    .catch((error) => {
+      gif.src = "https://media1.tenor.com/m/7hG_F7B2XccAAAAC/error.gif";
+      console.log(error);
     });
 };
 
